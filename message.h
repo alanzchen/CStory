216fce40f52c21c6b3c6c2ed34249e66882f6d12
Karashan <630250299@qq.com>
2017-04-19 16:54:42 +0800

    Update message.h

--- message.h
+++ message.h
@@ -9,6 +9,10 @@
 #include <iomanip>
 #include <time.h>
 #include <string>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <vector>
 #include "session.h"
 #include "story.h"
 
@@ -24,11 +28,41 @@ public:
      * TODO: just use "long timestamp = time(nullptr)" to get current timestamp
      *
      */
-    Message(std::string, long time, std::string url);
+    Message(std::string message_, long time, std::string url);   //directly use string for temporary message storing.
     long timestamp;
     std::string get_content();
     std::string get_session_id();
-    //std::bool operator<(time1, time2);
 };
 
+struct node
+{
+    friend bool operator< (node n1, node n2)
+    {
+         return n1.priority < n2.priority;
+    }
+    int priority;
+    int value;
+};
+
+std::priority_queue<message_pair,
+                    msg_container,
+                    decltype(time_comp)> queue(time_comp);
+
 #endif // MESSAGE_H
+
+/* Below is something not sure.
+
+bool time_comp = [](const message_pair& m1, const message_pair& m2);  //directly compare, no need this.
+
+
+class mycomparison{          //http://www.cplusplus.com/reference/queue/priority_queue/priority_queue/
+    bool reverse;
+
+public:
+    mycomparison(const bool& revparam=false) {reverse=revparam;}
+    bool operator() (const int& lhs, const int&rhs) const{
+        if (reverse) return (lhs>rhs);
+        else return (lhs<rhs);
+    }
+};
+*/
