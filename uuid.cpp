940f4a8f2b4b1e06441ede493c6b6d7100f6571f
Karashan <630250299@qq.com>
2017-05-12 17:13:43 +0800

    Introducing "uuid" to tackle some issues
    uuid.cpp updated
    Generate a unique uuid with functions of boost.

--- uuid.cpp
+++ uuid.cpp
@@ -1,29 +1,33 @@
 #include "uuid.h"
 
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+using namespace boost::uuids;
 
 
-string generate_uuid()
-{
-    uuid_t uu;
+//This is a another way to generate UUID by boost.
 
-    uuid_generate( uu );
+string generate_uuid(){
 
-    return uu;
+    boost::uuids::uuid a_uuid = boost::uuids::random_generator();
+
+    return a_uuid;
 
 }
 
 
 
-//Below is another plan for pringting the uuid out
-//void generate_uuid()
+
+//Below is another plan for getting a uuid
+
+//string generate_uuid()
 //{
 //    uuid_t uu;
-//        int i;
-//        uuid_generate( uu );
 
-//        for(i=0;i<16;i++)
-//        {
-//            printf("%02X-",uu[i]);
-//        }
-//        printf("\n");
+//    uuid_generate( uu );
+
+//    return uu;
+
 //}
+
