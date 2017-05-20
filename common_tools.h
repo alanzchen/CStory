96517f8904a8e69a0994d85f492bd2ec042a8e01
LucasChenZQ <LucasChenZQ@users.noreply.github.com>
2017-05-20 23:12:35 +0800

    add common_tools header

---
+++ common_tools.h
@@ -0,0 +1,14 @@
+//
+// Created by Lucas Chen on 2017/5/20.
+//
+
+#ifndef CSTORY_TOOLS_H
+#define CSTORY_TOOLS_H
+void negBool(bool &target) {
+	if (target) {
+		target = false;
+	} else {
+		target = true;
+	}
+}
+#endif //CSTORY_TOOLS_H
